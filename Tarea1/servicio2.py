e73dbc2a3ad87af6353a6b1f970905d47a4d2f09
Benjamin-Daza-Jimenez <benja.daza.jimenez@gmail.com>
2025-08-22 23:13:20 -0400

    goddddines

--- Tarea1/servicio2.py
+++ Tarea1/servicio2.py
@@ -13,37 +13,31 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((host, PORT_1_2))
-
-    #Escuchar conexiones: permitir conexiones entrantes
     s.listen()
 
-    #Aceptar conexiones: espera hasta que alguien se conecte, devuelve el host y el port de la conexion
     while  flag: 
         conn, addr = s.accept() 
-        datos=b""
         with conn:
             print('Conectado por', addr)
             while True:
                 data = conn.recv(1024*100) #Recibir hasta 1024 bytes, OJO con el tamaño del mensa
                 if not data:
                     break
-
-                datos+=data
                 print("Mensaje recibido:", data.decode())
-                if datos.decode() != "115":
-                    mensaje=crear_mensaje(datos.decode())
-                    sock.sendto(mensaje.encode(), (host, PORT_2_3))
-
+                
                 # Lógica de Finalización
-                partes = datos.decode().split("-")
+                partes = data.decode().split("-")
                 if partes[-1] == "FINALIZAR":
-
-                    sock.sendto(datos, (host, PORT_2_3))   
+                    print("aaaaaaaaaaaaaaaaaaaaaaaaaaa")
+                    sock.sendto(data, (host, PORT_2_3))   
                     flag=False
                     break
-
+                
+                if data.decode() != "115":
+                    mensaje=crear_mensaje(data.decode())
+                    sock.sendto(mensaje.encode(), (host, PORT_2_3))
                 else:
-                    mensaje=datos.decode()
+                    mensaje=data.decode()
                     sock.sendto(mensaje.encode('utf-8'), (host, PORT_2_3))
 
         if not flag:
